99807f03fa6d909c126470f1e3568f6d0036b04e
Teddy <etc.tortorici@gmail.com>
2024-10-01 13:27:37 -0600

    a

--- gixtpy2.py
+++ gixtpy2.py
@@ -1,7 +1,9 @@
 import numpy as np
 import fabio
+import yaml
 import pyFAI.detectors as detectors
 from scipy.optimize import curve_fit
+import pickle
 from pathlib import Path
 import matplotlib.pylab as plt
 from matplotlib.colors import LogNorm
@@ -9,34 +11,48 @@ import matplotlib
 matplotlib.rcParams['mathtext.fontset'] = 'cm'
 matplotlib.rcParams['font.family'] = 'STIXGeneral'
 
-class Data:
+class SpecularOmega:
 
     MAX_INT = (1 << 32) - 1
 
-    def __init__(self, data_directory):
+    def __init__(self, data_directory, det_dist=150, anglular_range=1.5, beam_width=1):
+        self.z0 = 0
+        self.det_dist=det_dist
+        self.beam_width = beam_width
         self.data_directory = data_directory
         self.instrument = None
         self.detector = None
         self.file_type = None
+        self.beam_center = None
         self.determine_instrument()
 
         self.file_list = list(data_directory.glob("*" + self.file_type))
 
-        self.pixels_above = 75   # number of pixels above the beam to keep (for crop)
-        self.pixels_below = 10   # number of pixels below the beam to keep (for crop)
-        self.pixel_horiontal_offset = 0
         self.pixel_size = self.detector.get_pixel1() * 1e3  # mm/pixel
+        self.pixels_above = int(det_dist * np.radians(anglular_range) / self.pixel_size)   # number of pixels above the beam to keep (for crop)
+        self.pixels_below = 8   # number of pixels below the beam to keep (for crop)
+        self.pixel_horiontal_offset = 0
+        self.z = np.arange(self.detector.shape[0])[::-1] * self.pixel_size
+        
         self.base_mask = np.logical_not(self.detector.calc_mask())
 
+        self.run()
+
+    def run(self):
         direct_beam_file_index = self.find_direct_beam_file_index()
 
-        self.beam_center = None
         if direct_beam_file_index is not None:
             self.beam_center = self.find_beam_center(direct_beam_file_index)
             print("Direct beam is at ({}, {})".format(*self.beam_center))
             del(self.file_list[direct_beam_file_index])
+        
+        try:
+            self.angles, self.intensity_data = self.load()
+        except:
+            self.angles,
 
-        self.angles, self.intensity_data = self.load()
+        self.process_data()
+        self.fit()
     
     def determine_instrument(self):
         num_edf_files = len(list(self.data_directory.glob("*.edf")))
@@ -81,7 +97,11 @@ class Data:
                                            p0=(0.75 * rows, 100, db_y.max()),
                                            nan_policy="omit")
         beam_center = (y0, x0)
-        # beam_center = np.unravel_index(np.argmax(intensity_db), intensity_db.shape)
+        to_write = {"beamcenter": {'x': float(x0), 'y': float(y0)},
+                    "sigma": {'x': float(sigma_x), 'y': float(sigma_y)},
+                    "amplitude": {'x': float(a_x), 'y': float(a_y)}}
+        with open(self.data_directory.parent / "beam_center.yaml", 'w') as yaml_file:
+            yaml.dump(to_write, yaml_file, default_flow_style=False)
         fig, ax = plt.subplots(1, 1)
         ax.set_facecolor('k')
         pos = ax.imshow(intensity_db, norm=LogNorm(1, intensity_db.max()))
@@ -109,29 +129,6 @@ class Data:
         intensity_data = intensity_data[sorting_args]
 
         return angles, intensity_data
-
-    def remove_crop(self):
-        pixels_above = 75   # number of pixels above the beam to keep (for crop)
-        pixels_below = 10   # number of pixels below the beam to keep (for crop)
-        pixel_horiontal_offset = 0
-
-    def crop(self, x_pixel_width, pixels_below, pixels_above, pixel_horiontal_offset=0):
-        self.x_pixel_width = x_pixel_width
-        self.pixels_below = pixels_below
-        self.pixels_above = pixels_above
-        self.pixel_horiontal_offset = pixel_horiontal_offset
-        #  db_loc_z = round(self.beam_center[0])
-        #  db_loc_x = round(self.beam_center[1])
-        #  z_lo = db_loc_z[0] - pixels_above
-        #  z_hi = db_loc_z[1] + pixels_below
-        #  x_ave = (db_loc_x[0] + db_loc_x[1]) / 2
-        #  
-        #  x_lo = int(x_ave - x_pixel_width * 0.5)
-        #  x_hi = round(x_ave + x_pixel_width * 0.5)
-        #  x_lo += x_offset
-        #  x_hi += x_offset
-        #  exposure_datas = self.intensity_data[:, z_lo:z_hi, x_lo:x_hi]
-        #  return exposure_datas
     
     def get_angle(self, filename: Path) -> float:
         if self.file_type == ".edf":
@@ -152,16 +149,19 @@ class Data:
         angle = round(angle, 3)
         return angle
     
-    def plot(self, beam_width=None, title=""):
+    def process_data(self):
         if self.beam_center is not None:
+            self.z -= (self.detector.shape[0] - self.beam_center[0]) * self.pixel_size
             bc_z = round(self.beam_center[0])
             z_lo = bc_z - self.pixels_above
             z_hi = bc_z + self.pixels_below
 
-            if beam_width is None:
+            self.z = self.z[z_lo:z_hi]
+
+            if self.beam_width is None:
                 data = self.intensity_data[:, z_lo:z_hi, :]
             else:
-                half_width = 0.5 * beam_width / self.pixel_size
+                half_width = 0.5 * self.beam_width / self.pixel_size
                 x_lo = round(self.beam_center[1] - half_width)
                 x_hi = round(self.beam_center[1] + half_width)
                 x_lo += self.pixel_horiontal_offset
@@ -169,18 +169,117 @@ class Data:
                 data = self.intensity_data[:, z_lo:z_hi, x_lo:x_hi]
         else:
             data = self.intensity_data
-        intensity_x_total = np.sum(data, axis=2)
-        print(intensity_x_total.max())
+        self.intensity_specular = np.sum(data, axis=2)
+        # print(self.intensity_specular.max())
 
+    def fit(self, z0=None, max_angle=1, pixel_cut=None):
+        if z0 is not None:
+            self.z0 = z0
+        print(f"z\u2080 = {self.z0}")
+        max_ind = np.argmax(self.intensity_specular, axis=0)
+        where_max_angle = self.angles[max_ind]
+
+        # Remove data below the beam center
+        valid = np.where(np.logical_and(
+            self.z > self.z0,
+            self.z < self.det_dist * np.radians(max_angle) + self.z0
+            ))
+        z = self.z[valid]
+        where_max_angle = where_max_angle[valid]
+        if pixel_cut is not None:
+            z = z[:-pixel_cut]
+            where_max_angle = where_max_angle[:-pixel_cut]
+        
+        (self.omega0, self.det_dist_fit), pcov = curve_fit(self.specular_fit, where_max_angle, z, p0=[0, self.det_dist])
+        perr = np.sqrt(np.diag(pcov))
+        print("Fit results:")
+        print(f"    \u03C9\u2080 = ({self.omega0} \u00B1 {perr[0]})\u00B0")
+        print(f"    d\u209B = ({self.det_dist_fit} \u00B1 {perr[1]}) mm")
+
+        fig, ax = plt.subplots(1, 1, figsize=(4, 3))
+
+        ax.scatter(where_max_angle, z, s=10, marker='o',
+                   edgecolors='k', lw=.75, facecolor='w')
+        omega = np.linspace(where_max_angle[-1] - 0.02, where_max_angle[0] + 0.02, 100)
+        ax.plot(omega, self.specular_fit(omega, self.omega0, self.det_dist_fit), "r")
+        ax.tick_params(axis='both', which='both', direction='in', right=True, top=True)
+        ax.set_ylabel("$z$ (mm)")
+        ax.set_xlabel("$\\omega$ motor position $(\\degree)$")
+        ax.grid(linestyle='dotted')
+        ax.xaxis.set_minor_locator(matplotlib.ticker.AutoMinorLocator())
+        ax.yaxis.set_minor_locator(matplotlib.ticker.AutoMinorLocator())
+        ax.set_title("where max pixel occurs")
+        # ax.legend(title="pixel")
+        annotation_text = f"$\\omega_0 = {self.omega0:.4f} \\pm {perr[0]:.4f}^\\circ$\n$d_{{sd}} = {self.det_dist_fit:.2f} \\pm {perr[1]:.2f}$ mm"
+        ax.text(0.05, 0.95, annotation_text, transform=ax.transAxes, fontsize=10,
+                verticalalignment='top', bbox=dict(facecolor='white', alpha=0.5))
+
+        fig.tight_layout()
+        return fig, ax
+
+    def specular_fit(self, omega, omega0, det_dist):
+        return det_dist * np.tan(2. * np.radians(omega - omega0)) + self.z0
+    
+    def zero_angle(self, omega, omega0, det_dist):
+        return det_dist * np.tan(np.radians(omega - omega0)) + self.z0
+    
+    def yoneda(self, omega, omega0, det_dist, critical_angle):
+        return det_dist * np.tan(np.radians(omega - omega0 + critical_angle)) + self.z0
+    
+    def refraction_pos1(self, omega, omega0, det_dist, critical_angle):
+        alpha = np.radians(omega - omega0)
+        alpha_sq = alpha * alpha
+        critical_angle = np.radians(critical_angle)
+        crit_sq = critical_angle * critical_angle
+        refraction_angle_sq = (alpha_sq - crit_sq) / (1 - 0.5 * crit_sq * alpha_sq)
+        return self.z0 + det_dist * np.tan(alpha - np.sqrt(refraction_angle_sq))
+    
+    def refraction_pos2(self, omega, omega0, det_dist, critical_angle):
+        alpha = np.radians(omega - omega0)
+        alpha_sq = alpha * alpha
+        critical_angle = np.radians(critical_angle)
+        crit_sq = critical_angle * critical_angle
+        refraction_angle_sq = (alpha_sq - crit_sq) / (1 - 0.5 * crit_sq * alpha_sq)
+        return self.z0 + det_dist * np.tan(alpha + np.sqrt(refraction_angle_sq))
+    
+    def refraction_neg(self, omega, omega0, det_dist, critical_angle):
+        alpha = np.radians(omega - omega0)
+        critical_angle = np.radians(critical_angle)
+        crit_sq = critical_angle * critical_angle
+        refraction_angle = np.sqrt(alpha * alpha * (1 - 0.5 * crit_sq) + crit_sq)
+        return self.z0 + det_dist * np.tan(alpha + refraction_angle)
+
+    def plot(self, title="", critical_angle=None, horizon=False):
         fig, ax = plt.subplots(1, 1, figsize=(4, 3))
         ax.set_facecolor('k')
 
-        z = (np.arange(intensity_x_total.shape[1])[::-1]) * self.pixel_size
+        # z = np.arange(intensity_x_total.shape[1])[::-1] * self.pixel_size
         ax.set_ylabel("Z (mm)")
 
-        color_map = ax.pcolormesh(self.angles, z, intensity_x_total.T,
-                                  norm=LogNorm(1, intensity_x_total.max()), cmap="plasma")
-
+        color_map = ax.pcolormesh(self.angles, self.z, self.intensity_specular.T,
+                                  norm=LogNorm(1, self.intensity_specular.max()), cmap="plasma")
+        omega2 = np.linspace(self.omega0, self.omega0 + .75, 1000)
+        ax.plot(omega2, self.specular_fit(omega2, self.omega0, self.det_dist_fit), "white", linewidth=1, alpha=0.5)
+        if horizon:
+            ax.plot(omega2, self.zero_angle(omega2, self.omega0, self.det_dist_fit), "white", linewidth=1, alpha=0.5)
+        if critical_angle is not None:
+            if isinstance(critical_angle, float):
+                critical_angle = [critical_angle]
+            last = None
+            for crit in critical_angle:
+                if crit == last:
+                    omega1 = np.linspace(self.omega0 + crit, self.omega0 + crit + .05, 100)
+                    ax.plot(omega1, self.refraction_pos2(omega1, self.omega0, self.det_dist_fit, crit), "white", linewidth=1, alpha=0.5)
+                else:
+                    ax.plot(omega2, self.yoneda(omega2, self.omega0, self.det_dist_fit, crit), "white", linewidth=1, alpha=0.5)
+                    omega1 = np.linspace(self.omega0 + crit, self.angles[-1], 1000)
+                    ax.plot(omega1, self.refraction_pos1(omega1, self.omega0, self.det_dist_fit, crit), "white", linewidth=1, alpha=0.5)
+                    omega1 = np.linspace(self.angles[0], self.omega0, 1000)
+                    ax.plot(omega1, self.refraction_neg(omega1, self.omega0, self.det_dist_fit, crit), "white", linewidth=1, alpha=0.5)
+                    
+                    # spec_at = self.specular_fit(crit + self.omega0, self.omega0, self.det_dist_fit)
+                    # ax.plot([crit + self.omega0, crit + self.omega0], [spec_at + 0.6, spec_at + 1], "white", linewidth=.5, alpha=0.5)
+                last = crit
         color_bar = fig.colorbar(color_map, ax=ax)
 
         ax.set_xlabel("$\\omega\\ (\\degree)$")
@@ -190,6 +289,27 @@ class Data:
         return fig, ax
 
 
+class SpecularZ(SpecularOmega):
+    def __init__(self, data_directory, det_dist=150):
+        self.bc_sigma = None
+        self.bc_amp = None
+        super().__init__(data_directory, det_dist, 0.5)
+        
+    def run(self):
+        with open(self.data_directory.parent / "beam_center.yaml", 'r') as yaml_file:
+            beam_center_data = yaml.safe_load(yaml_file)
+        self.beam_center = (beam_center_data["beamcenter"]['y'], beam_center_data["beamcenter"]['x'])
+        
+        sigma_x = beam_center_data["sigma"]['x']
+        a_x = beam_center_data["amplitude"]['x']
+        sigma_y = beam_center_data["sigma"]['y']
+        a_y = beam_center_data["amplitude"]['y']
+        print("Loaded beam center: ({}, {})".format(*self.beam_center))
+
 if __name__ == "__main__":
-    data = Data(Path(r"C:\Users\Teddy\OneDrive - UCB-O365\Rogerslab3\Teddy\TPP Films\BTB-TPP\2024 Film Growth\Film 2\XRD\non-grazing on blank for comparison\tune\raw_tiffs"))
+    # om = Data(Path(r"C:\Users\Teddy\OneDrive - UCB-O365\Rogerslab3\Teddy\TPP Films\BTB-TPP\2024 Film Growth\Film 2\XRD\non-grazing on blank for comparison\tune\raw_tiffs"))
+    path = Path(r"C:\Users\Teddy\OneDrive - UCB-O365\Rogerslab3\Teddy\TPP Films\BTB-TPP\2024 Film Growth\Film 4 (east campus dsc)\GIWAXS\film1\alignment")
+    spec = SpecularOmega(path / "spec")
+    spec.plot(critical_angle=0.24)
+    # z = SpecularZ(path / "z-scan")
     plt.show()
